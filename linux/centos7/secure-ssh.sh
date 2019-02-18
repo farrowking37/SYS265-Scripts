f92b605f76284758f33119604377c7f5c562fcdc
John Shultz <farrowking37@gmail.com>
2019-02-18 10:42:44 -0500

    Organized Directories

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+# SECURE-SSH.SH
+# Author Farrowking37
+# Createes a new SSH user called $1
+# Adds a public key to that users authorized keys file
+# Removes roots ability to ssh
+
+echo "testing 1 2 3"
