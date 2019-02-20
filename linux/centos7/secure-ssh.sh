b8cdfdb047d873968f4f58d3c7c73919437a5c78
John Shultz <farrowking37@gmail.com>
2019-02-20 11:47:45 -0500

    Updating secure-ssh.sh to final version

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,4 +4,22 @@
 # Adds a public key to that users authorized keys file
 # Removes roots ability to ssh
 
-echo "testing 1 2 3"
+
+# Potential code to create the user with a paramater for name
+sudo useradd $1
+sudo mkdir /home/$1/.ssh
+sudo cp /home/john/SYS265-Scripts/linux/public-keys/sys265.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+
+# Use printf to disable permit root login by appending the correct value to end of config
+sudo printf '\n%s\n' 'PermitRootLogin no' >>/etc/ssh/sshd_config
+
+# Use printf to enable pubkey authentication by appending the correct value to the end of config
+sudo printf '\n%s\n' 'PubkeyAuthentication yes'>>/etc/ssh/sshd_config
+
+# Restart ssh service to keep changes
+sudo systemctl restart sshd
+
+
